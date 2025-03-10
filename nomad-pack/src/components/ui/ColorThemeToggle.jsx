45270196277f8970c6292762338c77e8c8a83c66
Byron Farrell <byronfarrell@Byrons-MacBook-Pro.local>
2025-03-10 22:56:09 +0000

    ColorThemeToggle Feature

---
+++ nomad-pack/src/components/ui/ColorThemeToggle.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useTheme } from 'next-themes'
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'
+import {useEffect, useState} from "react";
+
+
+export default function ColorThemeToggle() {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false);
+
+    // Prevent hydration errors by delaying rendering until after mount
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
+
+    return (
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            {theme === "dark" ? (
+                <SunIcon className="text-contrast size-5" />
+            ) : (
+                <MoonIcon className="text-contrast size-5" />
+            )}
+        </button>
+    );
+}
